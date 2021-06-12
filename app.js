c96037e96bdf112ad8cd9a312726838049fd320a
ikethompson88 <ikethompson88@byui.edu>
2021-06-12 10:52:33 -0700

    Signup/Login Updates

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 
  const express = require('express');
  const bodyParser = require('body-parser');
+ const session = require('express-session');
+ const flash = require('connect-flash');
 
  const app = express();
 
@@ -10,13 +12,23 @@
  app.set('views', 'views');
 
  const mainRouter = require('./routes/main');
- const userRouter = require('./routes/user')
+ const userRouter = require('./routes/user');
+ const authRouter = require('./routes/auth');
 
  app.use(bodyParser.urlencoded({ extended: false }));
  app.use(express.static(path.join(__dirname, 'public')));
+ app.use(
+    session({
+      secret: 'my secret',
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+ app.use(flash());
 
  app.use(mainRouter);
  app.use(userRouter);
+ app.use(authRouter);
 
 
  app.listen(PORT);
