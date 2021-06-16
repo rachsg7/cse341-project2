cbc228a5018ba913bcc27734acfb2f2cf180ce78
Costigan-Stephen <stephenjcostigan@hotmail.com>
2021-06-15 21:54:55 -0500

    mongoose

--- app.js
+++ app.js
@@ -3,9 +3,16 @@
  const PORT = process.env.PORT || 5000;
 
  const express = require('express');
+ const mongoose = require('mongoose');
+ const mongoDBStore = require('connect-mongodb-session')(session);
  const bodyParser = require('body-parser');
  const session = require('express-session');
  const flash = require('connect-flash');
+
+ const MONGO_USER = process.env.MONGO_USER;
+ const MONGO_PASS = process.env.MONGO_PASS;
+
+ const MONGODB_URL = "mongodb+srv://" + MONGO_USER + ":" + MONGO_PASS + "@cluster0.2scof.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
  const app = express();
 
@@ -19,17 +26,23 @@
  app.use(bodyParser.urlencoded({ extended: false }));
  app.use(express.static(path.join(__dirname, 'public')));
  app.use(
-    session({
-      secret: 'my secret',
-      resave: false,
-      saveUninitialized: false,
-    })
-  );
+     session({
+         secret: 'my secret',
+         resave: false,
+         saveUninitialized: false,
+     })
+ );
  app.use(flash());
 
  app.use(mainRouter);
  app.use(userRouter);
  app.use(authRouter);
 
-
- app.listen(PORT);
+ mongoose
+     .connect(MONGODB_URL)
+     .then(result => {
+         app.listen(PORT);
+     })
+     .catch(err => {
+         console.log(err);
+     });
